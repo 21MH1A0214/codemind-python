4c1196b7dea30b52b85f6e057cd9b3bbc3d0f211
21MH1A0214 <98584411+21MH1A0214@users.noreply.github.com>
2023-05-26 14:07:53 +0530

    n = int(input()) l = list(map(int,input().split())) c = 0 for i in range(n):     if i%2==0:         c+=l[i] print(c)

--- Sum_of_Even_Positions.py
+++ Sum_of_Even_Positions.py
@@ -1,7 +1,7 @@
 n = int(input())
 l = list(map(int,input().split()))
-es = 0
-for i in range(0,len(l)):
-    if(i%2==0):
-        es = es+l[i]
-print(es)
+c = 0
+for i in range(n):
+    if i%2==0:
+        c+=l[i]
+print(c)
