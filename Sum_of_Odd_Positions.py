52b685f52fd4328b13cf83c16b054c381176e9b0
21MH1A0214 <98584411+21MH1A0214@users.noreply.github.com>
2023-05-26 14:06:22 +0530

    n = int(input()) l = list(map(int,input().split())) c = 0 for i in range(n):     if i%2!=0:         c+=l[i] print(c)

--- Sum_of_Odd_Positions.py
+++ Sum_of_Odd_Positions.py
@@ -1,7 +1,7 @@
 n = int(input())
 l = list(map(int,input().split()))
-os = 0
-for i in range(0,len(l)):
-    if(i%2!=0):
-        os = os+l[i]
-print(os)
+c = 0
+for i in range(n):
+    if i%2!=0:
+        c+=l[i]
+print(c)
