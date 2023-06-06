3eef62315f52960b6e3d19155c4b52299c6f19f9
21MH1A0214 <98584411+21MH1A0214@users.noreply.github.com>
2023-06-06 11:58:43 +0530

    def palin(n):     rev = 0     t = n     while n:         r = n%10         rev = rev*10+r         n = n//10     if t==rev:         return 1     else:         return 0 n = int(input()) f = palin(n) if f==1:     print(True) else:     print(False)

--- Palindrome.py
+++ Palindrome.py
@@ -1,8 +1,17 @@
+def palin(n):
+    rev = 0
+    t = n
+    while n:
+        r = n%10
+        rev = rev*10+r
+        n = n//10
+    if t==rev:
+        return 1
+    else:
+        return 0
 n = int(input())
-s = str(n)
-x = s[::-1]
-p = int(x)
-if p==n:
+f = palin(n)
+if f==1:
     print(True)
 else:
     print(False)
