2213c2a8110082c182d2df699a27c48b999c374a
21MH1A0214 <98584411+21MH1A0214@users.noreply.github.com>
2023-06-06 11:45:15 +0530

    def hpy(n):     x=n     sum1=0     while x>0:         c=x%10         sum1=(sum1+(c*c))         x//=10     return sum1 n=int(input()) hpy(n) s=n while s>9:     s=hpy(s) if  s==1 or s==7:     print(True) else:     print(False)

--- happy_number.py
+++ happy_number.py
@@ -1,14 +1,17 @@
-def sq(n):
-    t = 0
-    while n>0:
-        t+=pow(n%10,2)
-        n//=10
-    return t
-num = int(input())
-temp = num
-while temp!=1and temp!=4:
-    temp = sq(temp)
-if temp==1:
+def hpy(n):
+    x=n
+    sum1=0
+    while x>0:
+        c=x%10
+        sum1=(sum1+(c*c))
+        x//=10
+    return sum1
+n=int(input())
+hpy(n)
+s=n
+while s>9:
+    s=hpy(s)
+if  s==1 or s==7:
     print(True)
 else:
     print(False)
