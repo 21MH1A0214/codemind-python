9392826ceb6b44b50608efd330af60d931d72dd0
21MH1A0214 <98584411+21MH1A0214@users.noreply.github.com>
2023-06-09 10:25:06 +0530

    n = int(input()) a =  list(map(int,input().split())) b = set(a) c = 0 for i in b:     if a.count(i)==i:         c+=1 print(c)

--- value_same_as_count.py
+++ value_same_as_count.py
@@ -1,7 +1,7 @@
-n=int(input())
-a=list(map(int,input().split()))
-b=set(a)
-c=0
+n = int(input())
+a =  list(map(int,input().split()))
+b = set(a)
+c = 0
 for i in b:
     if a.count(i)==i:
         c+=1
